dd009b44b69c0ceebd338922a3d62698658ec1c9
Nebuloce <47251610+Nebuloce@users.noreply.github.com>
2019-07-10 19:44:38 -0230

    Small string change
    Had wrong string for default case. Updated

--- src/Dice.java
+++ src/Dice.java
@@ -100,7 +100,7 @@ public class Dice {
 				System.out.println("Good Bye");
 				break;
 		
-		default: System.out.println("Not a valid choice. Please choose between 0 and 7");
+		default: System.out.println("Not a valid choice.");
 				break;
 		}
 	}
